7ad842657ffc21e99377ad6c64fe07d5ced1e460
Leticia Mota <lfmota25@gmail.com>
2024-07-05 10:20:20 -0300

    Add files via upload

---
+++ TestePessoa.java
@@ -0,0 +1,11 @@
+public class TestePessoa {
+    public static void main(String[] args) {
+        
+        Pessoa p1 = new Pessoa("Camila",35, 1700.00);
+        
+        
+
+        p1.imprimeDados();//acesso bem sucedido
+    }
+    
+}
